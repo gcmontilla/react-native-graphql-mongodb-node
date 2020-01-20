277bb73529b4349956cc37a80eb7976257455d9d
Gregory Montilla <gcmontilla5@gmail.com>
2020-01-20 02:32:13 -0800

    Graphql api example

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { graphql, buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-    res.send('Hello World!');
-})
+const schema = `
+    type RootQuery {
+        events: [String!]!
+    }
+
+    type RootMutation {
+        createEvent(name: String): String
+    }
+
+    schema {
+        query: RootQuery
+        mutation: RootMutation
+    }
+`;
+
+app.use('/graphql', graphqlHttp({
+    schema: buildSchema(schema),
+    rootValue: {
+        events: () => {
+            return ['test', 'test-2']
+        },
+        createEvent: (args) => {
+            const eventName = args.name;
+            return eventName;
+        }
+    },
+    graphiql: true
+}));
 
 app.listen(3000);
 
